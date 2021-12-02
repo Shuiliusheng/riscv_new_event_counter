eab482f14730114a865398d44cf1dcdb469038e1
Shuiliusheng <cuihongwei@pku.edu.cn>
2021-12-02 12:39:09 +0800

    this test can read some value but cannot read 16-31

--- boom_scala/exu/execution-units/event_counter.scala
+++ boom_scala/exu/execution-units/event_counter.scala
@@ -12,7 +12,7 @@ import boom.util._
 
 class EventCounterIO(issueWidth: Int, writeWidth: Int)(implicit p: Parameters) extends BoomBundle
 {
-  val event_signals  = Input(Vec(32, UInt(4.W)))
+  val event_signals  = Input(Vec(16, UInt(4.W)))
 
   val read_addr = Input(Vec(issueWidth, Valid(UInt(5.W))))
   val read_data = Output(Vec(issueWidth, UInt(64.W)))
@@ -25,7 +25,16 @@ class EventCounterIO(issueWidth: Int, writeWidth: Int)(implicit p: Parameters) e
 class EventCounter(issueWidth: Int, writeWidth: Int)(implicit p: Parameters) extends BoomModule
 {
   val io = IO(new EventCounterIO(issueWidth, writeWidth))
-  val reg_counters = io.event_signals.zipWithIndex.map { case (e, i) => freechips.rocketchip.util.WideCounter(64, e, reset = false) }
+  val event_signals = Wire(Vec(32, UInt(4.W)))
+  for (i <- 0 until 16) {
+    event_signals(i) := io.event_signals(i)
+  }
+
+  for (i <- 16 until 32) {
+    event_signals(i) := 1.U
+  }
+
+  val reg_counters = event_signals.zipWithIndex.map { case (e, i) => freechips.rocketchip.util.WideCounter(64, e, reset = false) }
 
   //for reg counter value
   val reg_read_data = Reg(Vec(issueWidth, UInt(64.W)))
@@ -35,39 +44,39 @@ class EventCounter(issueWidth: Int, writeWidth: Int)(implicit p: Parameters) ext
 
   for (i <- 0 until issueWidth) {
     when (io.read_addr(i).valid) {
-      when (io.read_addr(i).bits === 0.U) { reg_read_data(i) := reg_counters(0) }
-      when (io.read_addr(i).bits === 1.U) { reg_read_data(i) := reg_counters(1) }
-      when (io.read_addr(i).bits === 2.U) { reg_read_data(i) := reg_counters(2) }
-      when (io.read_addr(i).bits === 3.U) { reg_read_data(i) := reg_counters(3) }
-      when (io.read_addr(i).bits === 4.U) { reg_read_data(i) := reg_counters(4) }
-      when (io.read_addr(i).bits === 5.U) { reg_read_data(i) := reg_counters(5) }
-      when (io.read_addr(i).bits === 6.U) { reg_read_data(i) := reg_counters(6) }
-      when (io.read_addr(i).bits === 7.U) { reg_read_data(i) := reg_counters(7) }
-      when (io.read_addr(i).bits === 8.U) { reg_read_data(i) := reg_counters(8) }
-      when (io.read_addr(i).bits === 9.U) { reg_read_data(i) := reg_counters(9) }
-      when (io.read_addr(i).bits === 10.U) { reg_read_data(i) := reg_counters(10) }
-      when (io.read_addr(i).bits === 11.U) { reg_read_data(i) := reg_counters(11) }
-      when (io.read_addr(i).bits === 12.U) { reg_read_data(i) := reg_counters(12) }
-      when (io.read_addr(i).bits === 13.U) { reg_read_data(i) := reg_counters(13) }
-      when (io.read_addr(i).bits === 14.U) { reg_read_data(i) := reg_counters(14) }
-      when (io.read_addr(i).bits === 15.U) { reg_read_data(i) := reg_counters(15) }
+      when (io.read_addr(i).bits === 0.U) { reg_read_data(i) := reg_counters(0).value }
+      when (io.read_addr(i).bits === 1.U) { reg_read_data(i) := reg_counters(1).value }
+      when (io.read_addr(i).bits === 2.U) { reg_read_data(i) := reg_counters(2).value }
+      when (io.read_addr(i).bits === 3.U) { reg_read_data(i) := reg_counters(3).value }
+      when (io.read_addr(i).bits === 4.U) { reg_read_data(i) := reg_counters(4).value }
+      when (io.read_addr(i).bits === 5.U) { reg_read_data(i) := reg_counters(5).value }
+      when (io.read_addr(i).bits === 6.U) { reg_read_data(i) := reg_counters(6).value }
+      when (io.read_addr(i).bits === 7.U) { reg_read_data(i) := reg_counters(7).value }
+      when (io.read_addr(i).bits === 8.U) { reg_read_data(i) := reg_counters(8).value }
+      when (io.read_addr(i).bits === 9.U) { reg_read_data(i) := reg_counters(9).value }
+      when (io.read_addr(i).bits === 10.U) { reg_read_data(i) := reg_counters(10).value }
+      when (io.read_addr(i).bits === 11.U) { reg_read_data(i) := reg_counters(11).value }
+      when (io.read_addr(i).bits === 12.U) { reg_read_data(i) := reg_counters(12).value }
+      when (io.read_addr(i).bits === 13.U) { reg_read_data(i) := reg_counters(13).value }
+      when (io.read_addr(i).bits === 14.U) { reg_read_data(i) := reg_counters(14).value }
+      when (io.read_addr(i).bits === 15.U) { reg_read_data(i) := reg_counters(15).value }
 
-      when (io.read_addr(i).bits === 16.U) { reg_read_data(i) := reg_counters(16) }
-      when (io.read_addr(i).bits === 17.U) { reg_read_data(i) := reg_counters(17) }
-      when (io.read_addr(i).bits === 18.U) { reg_read_data(i) := reg_counters(18) }
-      when (io.read_addr(i).bits === 19.U) { reg_read_data(i) := reg_counters(19) }
-      when (io.read_addr(i).bits === 20.U) { reg_read_data(i) := reg_counters(20) }
-      when (io.read_addr(i).bits === 21.U) { reg_read_data(i) := reg_counters(21) }
-      when (io.read_addr(i).bits === 22.U) { reg_read_data(i) := reg_counters(22) }
-      when (io.read_addr(i).bits === 23.U) { reg_read_data(i) := reg_counters(23) }
-      when (io.read_addr(i).bits === 24.U) { reg_read_data(i) := reg_counters(24) }
-      when (io.read_addr(i).bits === 25.U) { reg_read_data(i) := reg_counters(25) }
-      when (io.read_addr(i).bits === 26.U) { reg_read_data(i) := reg_counters(26) }
-      when (io.read_addr(i).bits === 27.U) { reg_read_data(i) := reg_counters(27) }
-      when (io.read_addr(i).bits === 28.U) { reg_read_data(i) := reg_counters(28) }
-      when (io.read_addr(i).bits === 29.U) { reg_read_data(i) := reg_counters(29) }
-      when (io.read_addr(i).bits === 30.U) { reg_read_data(i) := reg_counters(30) }
-      when (io.read_addr(i).bits === 31.U) { reg_read_data(i) := reg_counters(31) }
+      when (io.read_addr(i).bits === 16.U) { reg_read_data(i) := reg_counters(16).value }
+      when (io.read_addr(i).bits === 17.U) { reg_read_data(i) := reg_counters(17).value }
+      when (io.read_addr(i).bits === 18.U) { reg_read_data(i) := reg_counters(18).value }
+      when (io.read_addr(i).bits === 19.U) { reg_read_data(i) := reg_counters(19).value }
+      when (io.read_addr(i).bits === 20.U) { reg_read_data(i) := reg_counters(20).value }
+      when (io.read_addr(i).bits === 21.U) { reg_read_data(i) := reg_counters(21).value }
+      when (io.read_addr(i).bits === 22.U) { reg_read_data(i) := reg_counters(22).value }
+      when (io.read_addr(i).bits === 23.U) { reg_read_data(i) := reg_counters(23).value }
+      when (io.read_addr(i).bits === 24.U) { reg_read_data(i) := reg_counters(24).value }
+      when (io.read_addr(i).bits === 25.U) { reg_read_data(i) := reg_counters(25).value }
+      when (io.read_addr(i).bits === 26.U) { reg_read_data(i) := reg_counters(26).value }
+      when (io.read_addr(i).bits === 27.U) { reg_read_data(i) := reg_counters(27).value }
+      when (io.read_addr(i).bits === 28.U) { reg_read_data(i) := reg_counters(28).value }
+      when (io.read_addr(i).bits === 29.U) { reg_read_data(i) := reg_counters(29).value }
+      when (io.read_addr(i).bits === 30.U) { reg_read_data(i) := reg_counters(30).value }
+      when (io.read_addr(i).bits === 31.U) { reg_read_data(i) := reg_counters(31).value }
     }
   }
 
