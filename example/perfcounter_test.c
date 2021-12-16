dfd6d73f0cfca281fe8cb190c16bcad62d72e620
Shuiliusheng <cuihongwei@pku.edu.cn>
2021-12-16 13:15:35 +0800

    improved the pfc_asm.h: the start_record and exit_record will be runned before and after main

--- example/perfcounter_test.c
+++ example/perfcounter_test.c
@@ -4,15 +4,6 @@
 
 int test()
 {
-	
-	long before[32]={0};
-	long after[32]={0};
-	int n=0;
-	RESET_COUNTER();
-	for(n=0;n<32;n++){
-		before[n]=read_counter(n);
-	}
-
 	//start testing .....
 	for(int i=0;i<30;i++){
 		int *arr = (int *)malloc(sizeof(int)*1024);
@@ -24,32 +15,12 @@ int test()
 		}
 	}
 	//end testing .....
-
-	for(n=0;n<32;n++){
-		after[n]=read_counter(n);
-	}
-
-	for(n=0;n<32;n++){
-		printf("event %2d: before testing: %llu, after testing: %llu, before-after: %llu\n", n, before[n], after[n], after[n]-before[n]);
-	}
 	return 0;
 }
 
 int main()
 {
-	printf("test: event set 0\n");
-	set_event_set(0);
 	test();
-
-
-	printf("test: event set 1\n");
-	set_event_set(1);
-	test();
-
-	printf("test: event set 2\n");
-	set_event_set(2);
-	test();
-	
 	return 0;
 }
 	
