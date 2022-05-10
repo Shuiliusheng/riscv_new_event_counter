3b00677802890a8d67542e6c0753d72ddf41f42c
shuiliusheng <cuihongwei@pku.edu.cn>
2022-05-10 13:14:08 +0000

    supplied a example case which includes an headfile pfc_define.h

--- example/perfcounter_test.c
+++ example/perfcounter_test.c
@@ -1,17 +1,13 @@
 #include<stdio.h>
 #include<stdlib.h>
-#include"pfc_asm.h"
+#include"pfc_define.h"
 
 int test()
 {
-	//start testing .....
-	for(int i=0;i<30;i++){
-		int *arr = (int *)malloc(sizeof(int)*1024);
-		for(int c=0;c<1024;c++)
-			for(int j=0;j<104;j++){
-			int c=rand()%5;
-			if(c>2)
-				arr[(c+j)%1024]=1;
+	int arr[10240];
+	for(int i=0;i<100;i++){
+		for(int c=0;c<200;c++){
+			arr[(i*c)%10240]=i+c;	
 		}
 	}
 	//end testing .....
